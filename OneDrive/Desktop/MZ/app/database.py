b806a1dc4beab9dfe2874b9808c26f568593d30c
Gleb1371 <163561026+Gleb1371@users.noreply.github.com>
2024-10-05 22:56:54 +0700

    Delete OneDrive/Desktop/MZ directory

--- OneDrive/Desktop/MZ/app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-from databases import Database
-
-DATABASE_URL = "postgresql+asyncpg://postgres:password123@localhost:5432/tasks"
-
-database = Database(DATABASE_URL)
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
